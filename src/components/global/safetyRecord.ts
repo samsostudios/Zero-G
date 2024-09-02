9d87284e3c14a31d0a4fbe94e5bfd42a0c9a6311
samsostudios <sam@samsostudios.com>
2024-09-02 16:45:11 -0600

    Added - Safety Module count up animation

---
+++ src/components/global/safetyRecord.ts
@@ -0,0 +1,66 @@
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+export const safetyRecord = () => {
+  console.log('SAFETY');
+
+  gsap.registerPlugin(ScrollTrigger);
+
+  class SafetyStatsCounter {
+    private safetyStats: NodeListOf<HTMLElement>;
+
+    constructor() {
+      this.safetyStats = document.querySelectorAll('.safety_stat h3');
+      this.init();
+    }
+
+    // Initialize Values and Get Target
+    private init() {
+      this.safetyStats.forEach((stat) => {
+        const targetValue = this.extractNumber(stat.textContent || '');
+
+        stat.innerHTML = '0';
+
+        // Add ScrollTrigger to the animation
+        ScrollTrigger.create({
+          trigger: '.section_safety',
+          start: 'top 80%',
+          onEnter: () => this.animateCount(stat, targetValue),
+          once: true,
+          //   markers: true,
+        });
+      });
+    }
+
+    // Clean Text for Target Value
+    private extractNumber(text: string): number {
+      const cleanedText = text.replace(/[^\d,]/g, '');
+      return parseInt(cleanedText.replace(/,/g, ''), 10);
+    }
+
+    // Animate number to Target Value
+    private animateCount(element: HTMLElement, targetValue: number) {
+      gsap.to(element, {
+        innerHTML: targetValue,
+        duration: 2,
+        onUpdate: () => {
+          if (element) {
+            const roundedNumber = Math.ceil(Number(element.textContent));
+            element.innerHTML = this.numberWithCommas(roundedNumber) + '+';
+          }
+        },
+        ease: 'power1.inOut',
+        snap: { innerHTML: 1 },
+      });
+    }
+
+    // Set output value to have commas
+    private numberWithCommas(x: number): string {
+      return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+    }
+  }
+
+  new SafetyStatsCounter();
+};
+
+export default safetyRecord;
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,25 @@
-import { home } from './pages/home';
+// import { safetyRecord } from '$gComponents/safetyRecord';
+import { home } from '$pages/home';
 
 window.Webflow ||= [];
 window.Webflow.push(() => {
   console.log('/// mainJS ///');
 
+  // Global Components
+  // ----------------
+  function initComponent(selector: string, importModule: () => Promise<{ default: () => void }>) {
+    const element = document.querySelector(selector);
+    if (element) {
+      importModule().then((module) => {
+        module.default(); // Assuming the module has a default export
+      });
+    }
+  }
+
+  initComponent('.section_safety', () => import('$gComponents/safetyRecord'));
+
   // Page Routing
+  // ------------
   function loadPageModules() {
     const pathname = window.location.pathname as string;
 
